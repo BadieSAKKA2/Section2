9a8511a42834c73f9c56a21bb376d405f9a7bbf9
medtaher123 <56443200+medtaher123@users.noreply.github.com>
2023-11-27 18:14:39 +0100

    added header

---
+++ food-delivery/src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import "./Header.css";
+import cartIcon from "../../assets/cart-shopping-solid.svg";
+//import cartIcon from "../../assets/cart.png";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header-title">ReactMeals</div>
+      <button id="cart-button" className="cart-button">
+        <img src={cartIcon} alt="Cart Icon" className="cart-icon" />
+        <div className="cart-button-text">Your Cart</div>
+        <div className="cart-number-tag">2</div>
+      </button>
+    </div>
+  );
+}
+
+export default Header;
