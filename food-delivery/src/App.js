e825ed675126f8836ca27220351b0f2e1798a7bc
youssefab-11 <youssef.abbes111@gmail.com>
2023-11-28 23:46:55 +0100

    making item list

--- food-delivery/src/App.js
+++ food-delivery/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ItemsList from './Components/Items/ItemsList';
 
 function App() {
   return (
     <div className="App">
-
+      <h2>Hello world!</h2>
+      <ItemsList />
     </div>
   );
 }
---
+++ food-delivery/src/Components/Items/ItemsList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Item from './Item';
+
+import './ItemsList.css';
+
+const ItemsList = () => {
+    const Items=[
+        {id:'e1', label:'Sushi' , desc:'Finest fish and veggies', price: 22.99},
+        {id:'e2', label:'Schnitzel' , desc:'A german specialty', price: 16.50},
+        {id:'e3', label:'Barbecue Burger' , desc:'American, raw, meaty', price: 12.99},
+        {id:'e4', label:'Green Bowl' , desc:'Healthy...and green...', price: 18.99}
+    ];
+
+    return(
+        <div className='items-list'>
+            {Items.map( (item) => (
+            <Item 
+                key={item.id}
+                title={item.label}
+                desc={item.desc}
+                price={item.price}
+            />
+            ))}
+        </div>
+        
+    )
+}
+
+export default ItemsList;
